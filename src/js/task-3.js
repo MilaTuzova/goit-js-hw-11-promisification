84e06398e3d1ab3e161758730c3a947ebcd5ef0f
MilaTuzova <tuzovals@gmail.com>
2021-07-11 18:33:00 +0300

    added task 3

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -4,18 +4,21 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = (transaction) => {
     const delay = randomIntegerFromInterval(200, 500);
 
-    setTimeout(() => {
-        const canProcess = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
 
-        if (canProcess) {
-            onSuccess(transaction.id, delay);
-        } else {
-            onError(transaction.id);
-        }
-    }, delay);
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+
+            if (canProcess) {
+                resolve(transaction.id, delay);
+            } else {
+                reject(transaction.id);
+            }
+        }, delay);
+    });
 };
 
 const logSuccess = (id, time) => {
@@ -26,16 +29,6 @@ const logError = id => {
     console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
     .then(logSuccess)
     .catch(logError);
@@ -51,3 +44,40 @@ makeTransaction({ id: 72, amount: 75 })
 makeTransaction({ id: 73, amount: 100 })
     .then(logSuccess)
     .catch(logError);
+
+
+
+// // _________________________________
+// /*
+//  * Работает так
+//  */
+// const randomIntegerFromInterval = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+// const makeTransaction = (transaction, onSuccess, onError) => {
+//     const delay = randomIntegerFromInterval(200, 500);
+
+//     setTimeout(() => {
+//         const canProcess = Math.random() > 0.3;
+
+//         if (canProcess) {
+//             onSuccess(transaction.id, delay);
+//         } else {
+//             onError(transaction.id);
+//         }
+//     }, delay);
+// };
+
+// const logSuccess = (id, time) => {
+//     console.log(`Transaction ${id} processed in ${time}ms`);
+// };
+
+// const logError = id => {
+//     console.warn(`Error processing transaction ${id}. Please try again later.`);
+// };
+
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
